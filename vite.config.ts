22ec3a0135994c7af08e07b2d48001a28243d76f
Roberto C Navarro <rnavarro70@gmail.com>
2025-12-07 18:09:19 -0500

    update, fix db connection no more neon-vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import neon from './neon-vite-plugin.ts'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    neon,
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
